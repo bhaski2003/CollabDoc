49bddc823065a0675c495296ad32d190b8611aab
Bhaskar KR <95578571+bhaski2003@users.noreply.github.com>
2024-09-19 10:41:44 +0530

    Create server.js

---
+++ server/server.js
@@ -0,0 +1,53 @@
+import dotenv from "dotenv";
+import { Server } from "socket.io";
+import mongoose from "mongoose";
+import Document from "./dbDocument.js";
+
+dotenv.config({ path: "../.env" });
+
+mongoose
+  .connect(process.env.PROJ_DB_CONNECTION_STRING)
+  .then(() => console.log("mongodb connected!"))
+  .catch(err => console.log("mongodb error", err));
+
+let CORS_ACCESSIBLE_SITE = process.env.PROJ_DEPLOYED_FRONTEND_URL;
+
+if (process.env.MODE === "development" || process.env.MODE === "dev") {
+  CORS_ACCESSIBLE_SITE = "*";
+}
+
+const conn = new Server(Number(process.env.PROJ_SERVER_PORT), {
+  cors: {
+    origin: CORS_ACCESSIBLE_SITE,
+    methods: ["GET", "POST"],
+  },
+});
+
+conn.on("connection", socket => {
+  socket.on("get-document", async id => {
+    const document = await findOrCreateDocument(id);
+    socket.join(id);
+
+    // Ensure the document exists before accessing `data`
+    if (document) {
+      socket.emit("load-document", document.data);
+    } else {
+      socket.emit("load-document", ""); // Send empty data if document is undefined
+    }
+
+    socket.on("send-changes", delta => {
+      socket.broadcast.to(id).emit("receive-changes", delta, id);
+    });
+
+    socket.on("save-document", async data => {
+      await Document.findByIdAndUpdate(id, { data });
+    });
+  });
+});
+
+async function findOrCreateDocument(id) {
+  if (id == null) return null;
+  const document = await Document.findById(id);
+  if (document) return document;
+  return await Document.create({ _id: id, data: "" });
+}
